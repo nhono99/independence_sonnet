df235c8b8fcc7340588b28e1e82c2f9c7630c727
DOGBEARDDELTA\Ronald <nhonoskywalker@yahoo.com>
2018-03-26 20:24:08 +0800

    main nav window click event
    mobile nav can now be close when clicked outside the nav dropdown

--- ico_playbunk_front_end/resources/js/script.js
+++ ico_playbunk_front_end/resources/js/script.js
@@ -1,26 +1,55 @@
 $(document).ready(function(){
+    /*FOR on nav outside click close nav dropdown */
+    var mobile_nav_modal = document.getElementById('mobile-nav-modal');
+    var main_nav_container = document.querySelector(".main-nav-container");
      /*mobile nav*/
     $('.js--nav-icon').click(function(){
+        navClick();
+    });
+    
+    function navClick()
+    {
         var nav = $('.js--main-nav');
         var icon = $('.js--nav-icon i');
         nav.slideToggle(200);
         if(icon.hasClass('ion-navicon-round'))
+        {
+           icon.addClass('ion-close-round');
+           icon.removeClass('ion-navicon-round');
+
+           if(!$('nav').hasClass('sticky'))
            {
-               icon.addClass('ion-close-round');
-               icon.removeClass('ion-navicon-round');
-               
-               if(!$('nav').hasClass('sticky'))
-               {
-                   $('nav').addClass('nav-container');
-               }
+               $('nav').addClass('nav-container');
            }
+        
+            mobile_nav_modal.style.left = '0';
+        }
         else
-            {
-                icon.removeClass('ion-close-round');
-                icon.addClass('ion-navicon-round');
-                $('nav').removeClass('nav-container');
-            }
-    });
+        {
+            icon.removeClass('ion-close-round');
+            icon.addClass('ion-navicon-round');
+            $('nav').removeClass('nav-container');
+            mobile_nav_modal.style.left = '100%';
+           
+        }
+    }
+    
+    window.addEventListener("click", mobile_nav_modal_Click);
+    window.addEventListener("click", main_nav_container_Click);
+    
+    function mobile_nav_modal_Click(e){
+      if(e.target == mobile_nav_modal){
+          mobile_nav_modal.style.left = '100%';
+          navClick();
+      }
+    }
+    function main_nav_container_Click(e){
+      if(e.target == main_nav_container){
+          navClick();
+          mobile_nav_modal.style.left = '100%';
+      }
+    }
+    
     
     /*----------------------------------------------------------------------------------------------*/
     /*MAIN-NAV smooth scrolling snippet from https://css-tricks.com/snippets/jquery/smooth-scrolling/
